019aeddd325b882ce98afe8c86fcb6c1652806e9
disco <leonziegenhagen22@gmail.com>
2025-04-02 20:59:14 +0200

    add postgresql integration

--- src/main/java/com/discohagen/springventory/SpringventoryApplication.java
+++ src/main/java/com/discohagen/springventory/SpringventoryApplication.java
@@ -19,21 +19,21 @@ public class SpringventoryApplication {
     @Bean
     public CommandLineRunner loadData(ItemRepository itemRepository, LocationRepository locationRepository) {
         return (args) -> {
-            Location warehouse = new Location(null, "Warehouse", "Main warehouse", null, null, null);
-            Location store = new Location(null, "Store", "A retail store", null, null, null);
+            Location warehouse = new Location(null, "Warehouse", "Main warehouse", null, null, null, null, null);
+            Location store = new Location(null, "Store", "A retail store", null, null, null, null, null);
             locationRepository.save(warehouse);
             locationRepository.save(store);
 
-            Location sectionA = new Location(null, "Section A", "Section A of the warehouse", warehouse, null, null);
+            Location sectionA = new Location(null, "Section A", "Section A of the warehouse", warehouse, null, null, null, null);
             locationRepository.save(sectionA);
 
-            Item smartphone = new Item(null, "Smartphone", "A smart phone", 10, warehouse);
-            Item mouse = new Item(null, "Mouse", "A computer mouse", 55, warehouse);
-            Item keyboard = new Item(null, "Keyboard", "A computer keyboard", 13, warehouse);
-            Item desktop = new Item(null, "Desktop", "A desktop pc", 20, store);
-            Item cable = new Item(null, "Cable", "A power cable", 44, store);
-            Item laptop = new Item(null, "Laptop", "A Laptop", 30, sectionA);
-            Item powerBrick = new Item(null, "Power Brick", "A power brick", 12, sectionA);
+            Item smartphone = new Item(null, "Smartphone", "A smart phone", 10, warehouse, null, null);
+            Item mouse = new Item(null, "Mouse", "A computer mouse", 55, warehouse, null, null);
+            Item keyboard = new Item(null, "Keyboard", "A computer keyboard", 13, warehouse, null, null);
+            Item desktop = new Item(null, "Desktop", "A desktop pc", 20, store, null, null);
+            Item cable = new Item(null, "Cable", "A power cable", 44, store, null, null);
+            Item laptop = new Item(null, "Laptop", "A Laptop", 30, sectionA, null, null);
+            Item powerBrick = new Item(null, "Power Brick", "A power brick", 12, sectionA, null, null);
             itemRepository.save(smartphone);
             itemRepository.save(mouse);
             itemRepository.save(keyboard);
